f63b6d18b3c53938273a16a021c60e90a3f16b84
FatineBts <fatinebentires@gmail.com>
2018-11-22 20:59:39 +0100

    q1 finie ok

--- GPU/neutron-gpu.cu
+++ GPU/neutron-gpu.cu
@@ -8,19 +8,24 @@
 #include <curand_kernel.h>
 
 #define OUTPUT_FILE "/tmp/3302011/absorbed.dat"
-#define NbpaquetN 512
+#define NB_THREADS_PER_BLOCK 512
 
 char info[] = "\
 Usage:\n\
-    neutron-gpu H Nb C_c C_s\n\
+    neutron-gpu H Nb C_c C_s nbthreads NbBlocks.x\n\
 \n\
     H  : épaisseur de la plaque\n\
     Nb : nombre d'échantillons\n\
     C_c: composante absorbante\n\
     C_s: componente diffusante\n\
+    nbthreads : nombre de threads\n\
+    NbBlocks.x : dimension en x d'un bloc\n\
 \n\
-Exemple d'execution : \n\
-    neutron-gpu 1.0 500000000 0.5 0.5\n\
+Exemple d'execution :\n\
+    neutron-gpu 1.0 500000000 0.5 0.5 256 256\n\
+\n\
+Remarque :\n\
+    /!\\ il n'est pas possible de prendre nbthreads > 512 et NbBlocks.x > 1024\n\
 ";
 
 /*
@@ -35,19 +40,8 @@ double my_gettimeofday(){
 __global__ void setup_kernel(curandState* state, unsigned long seed)
 {
     int id_thread = threadIdx.x + blockIdx.x*blockDim.x;
-    //threadIx.x = numéro du thread qui va de 0 à la dimension du bloc -1
-    //blockId = va correspondre au numéro du bloc qu'on considère 
-    //blockDim = dimension du bloc (nombre de cases dans une direction donc si on considère qu'une case = 1 thread, donne le nbr de threads par bloc)
-    //gridDim = donne le nombre de blocs dans une grille (n/NbTheadsParBloc)
-    //gridDim*blockDim = nombre de threads dans une grille = n
-
-    //va donner par exemple : 0+0*5 (0),..,4+0*5 (4, dernier element du premier bloc),0+1*5 (5),...,4+1*5 (9, dernier element du second bloc)
     curand_init(seed, id_thread, 0, &state[id_thread]);
 }
-
-/*
-  Fonction qui va servir à générer des nombres différents pour chaque thread. 
-*/
 
 __device__ float generate(curandState* globalState, int ind) 
 {
@@ -57,7 +51,7 @@ __device__ float generate(curandState* globalState, int ind)
     return random;
 }
 
-__global__ void kernel(curandState* globalState, float* absorbed, float h, int n, float c, float c_c, float c_s, int paquetN, int* result) //uniquement les elements qui sont intialisés dans le main + r b et t
+__global__ void kernel(curandState* globalState, float* absorbed, float h, int n, float c, float c_c, float c_s, int* result) //uniquement les elements qui sont intialisés dans le main + r b et t
 {
   float d; 
   float x; 
@@ -66,14 +60,12 @@ __global__ void kernel(curandState* globalState, float* absorbed, float h, int n
   int i = blockDim.x*blockIdx.x + threadIdx.x; //sert de compteur
   int gi = i;
   int prev;
-  __shared__ int r_local[NbpaquetN]; //taille du nombre de threads qu'il va nous falloir pour traiter paquetN neutrons par thread, comme on a imposé 512 threads par bloc dans le main, NbpaquetN vaudra 512, on aura donc 512 threads qui vont traiter chacun 1 paquetN donc 512 paquetN. Il s'agit du nombre de paquetN.  
-  __shared__ int t_local[NbpaquetN]; 
-  __shared__ int b_local[NbpaquetN];
+  __shared__ int r_local[NB_THREADS_PER_BLOCK]; 
+  __shared__ int t_local[NB_THREADS_PER_BLOCK]; 
+  __shared__ int b_local[NB_THREADS_PER_BLOCK];
   r_local[threadIdx.x] = 0; //on initialise à zéro le tableau
   t_local[threadIdx.x] = 0; 
-  b_local[threadIdx.x] = 0; 
-  //int r_updated = 0, t_updated = 0, b_updated = 0;  
-  int r, t, b; 
+  b_local[threadIdx.x] = 0;  
 
   while(i<n){ //i doit s'incrémenter mais pas gi
   d = 0.0; 
@@ -101,12 +93,9 @@ __global__ void kernel(curandState* globalState, float* absorbed, float h, int n
       d = u * M_PI; //direction
     } 
   } //boucle while(1)
-  i += (gridDim.x*blockDim.x); //nombre de threads dans une grille qui correspond ici à un bloc, on fait des sauts correspondants aux nombres de threads dans un bloc ce qui donne 512 
+  i += (gridDim.x*blockDim.x);
 }//while(i<n) //tant qu'on a pas traité tous les neutrons 
 
- r_local[threadIdx.x] = r; 
- t_local[threadIdx.x] = t; 
- b_local[threadIdx.x] = b; 
   __syncthreads(); //synchronize the local threads writing to the local memory cache 
 
   int j = blockDim.x / 2; 
@@ -143,14 +132,17 @@ int main(int argc, char *argv[]) {
   float h;
   int r, b, t;
   int n,j; 
-  int paquetN;
+  int nbthreads;
+  dim3 NbBlocks;
   
     // valeurs par defaut
   h = 1.0;
   n = 500000000;
   c_c = 0.5;
   c_s = 0.5;
-
+  nbthreads = 256;
+  NbBlocks.x = 256;
+  
   // recuperation des parametres
   if (argc > 1)
     h = atof(argv[1]);
@@ -161,7 +153,10 @@ int main(int argc, char *argv[]) {
   if (argc > 4)
     c_s = atof(argv[4]);
   if (argc > 5)
-     paquetN = atof(argv[5]);
+    nbthreads = atof(argv[5]);
+  if (argc > 6)
+    NbBlocks.x = atof(argv[6]);
+
   c = c_s + c_c; 
   r = b = t = j = 0;
 
@@ -174,20 +169,17 @@ int main(int argc, char *argv[]) {
   float* absorbed_GPU;
   int* result_CPU; 
   int* result_GPU;
-  //il s'agit du nombre de neutrons traités par 1 thread = nombres de neutrons dans un paquet. 
   curandState* devStates;
-  printf("paquetN : %d\n", paquetN);
 
   /* Définition du nombre de threads et de la taille de la grille */
-  dim3 NbThreadsParBloc(NbpaquetN,1,1); dim3 NbBlocks;  
-
-  NbBlocks.x = NbpaquetN;//iDivUp(iDivUp(n,paquetN),NbThreadsParBloc.x); //on fait en sorte qu'au lieu qu'un thread traite 1 neutron, 1 thread va en traiter paquetN. On impose le nombre de threads par blocs à 512 et on cherche le nombre de blocs qu'il faudrait si on a n neutrons avec un paquet de neutrons traité par 1 thread égal à paquetN. Plus on augmente paquetN et plus n est petit et à priori plus la vitesse d'execution devrait être élevée.   
+  dim3 NbThreadsParBloc(nbthreads,1,1);
   
+  printf("nombre de threads : %4.2d\n",nbthreads);
+  printf("nombre de threads par bloc : %4.2d\n",NB_THREADS_PER_BLOCK);
+  printf("nombre de blocs : %4.2d\n",NbBlocks.x);
 
-  //printf("n/paquetN %d\n",iDivUp(n,paquetN));
-  printf("nombre de blocs %4.2d\n",NbBlocks.x);
   NbBlocks.y = 1;
-  NbBlocks.z = 1;  
+  NbBlocks.z = 1; 
 
   /* Allocation de la mémoire */
   absorbed_CPU = (float *) calloc(n,sizeof(float)); //sur CPU 
@@ -203,7 +195,7 @@ int main(int argc, char *argv[]) {
   start = my_gettimeofday();
 
   setup_kernel <<<NbBlocks,NbThreadsParBloc>>> (devStates,unsigned(time(NULL)));  //initialisation de l'état curandState pour chaque thread
-  kernel<<<NbBlocks, NbThreadsParBloc>>>(devStates, absorbed_GPU, h, n, c, c_c, c_s, paquetN, result_GPU); //génération des positions absorbed pour GPU
+  kernel<<<NbBlocks, NbThreadsParBloc>>>(devStates, absorbed_GPU, h, n, c, c_c, c_s, result_GPU); //génération des positions absorbed pour GPU
    //on renvoie aussi r, t et b pour l'affichage plus loin dans le code 
   
   cudaMemcpy(absorbed_CPU, absorbed_GPU, n*sizeof(float), cudaMemcpyDeviceToHost); //copie du absorbed GPU dans CPU 
